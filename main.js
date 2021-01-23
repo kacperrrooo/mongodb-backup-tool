460a33b89b5fe4bef858ade77125c2ae32274cbd
kacperrrooo <62256822+kacperrrooo@users.noreply.github.com>
2021-01-24 00:41:11 +0100

    mongodb-backup-tool 1.0.0

---
+++ main.js
@@ -0,0 +1,64 @@
+const { writeFile, existsSync } = require("fs");
+const mongoose = require("mongoose");
+const input = require("input");
+
+const qMarks = (objects) => {
+	const l = objects.length;
+	const marks = [];
+	for (let i = 0; i < l; i++) {
+		marks.push("?");
+	}
+
+	return marks;
+}
+
+input.text("Podaj nazwe pliku do którego ma zostać wgrany backup", { default: "backup" }).then(async file => {
+	const ip = await input.text("Podaj ip serwera bazy danych (bez portu!)", { default: "localhost" });
+	const port = await input.text("Podaj port bazy danych", { default: "27017" });
+	const databse = await input.text("Podaj nazwę bazy danych której chcesz zrobić backup");
+	const url = `mongodb://${ip}:${port}/${databse}`;
+
+	const connection = await mongoose.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	});
+
+	const collections = await connection.connection.db.listCollections().toArray();
+
+	if (!existsSync(`./${file}.db`)) writeFile(`./${file}.db`, "", () => { });
+
+	const db = require("better-sqlite3")(`./${file}.db`);
+
+	await new Promise(resolve => {
+		collections.forEach(async (c, ci) => {
+			const collection = await connection.connection.db.collection(c.name);
+			let data = await collection.find({});
+			data = await data.toArray();
+			await new Promise(r => {
+				data.forEach((d, i) => {
+					const keys = Object.keys(d).filter(x => x !== "_id" && x !== "__v");
+					if (i === 0) {
+						const sql = `CREATE TABLE IF NOT EXISTS ${c.name} (${keys.join(",")})`;
+						db.prepare(sql).run();
+					}
+		
+					
+					const objects = [];
+		
+					keys.forEach(k => {
+						objects.push(d[k]);
+					});
+		
+					db.prepare(`INSERT INTO ${c.name} (${keys.join(",")}) VALUES(${qMarks(objects).join(",")})`).run(...objects);
+					if (i === data.length - 1) r();
+				});
+			});
+			if (ci === collections.length - 1) resolve();
+		});
+	});
+	const close = await input.text("Backup zakończony pomyślnie czy chcesz zamknąć program? (Y/N)", { default: "Y" });
+	if (close.toLowerCase() === "y") process.exit();
+});
+
